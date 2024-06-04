39c1e20ea2ca171b0c7d99b26e54148d9eaf8309
unknown <tadongguk@gmail.com>
2024-06-05 08:44:52 +0900

    update_excercise_1

--- week1_exercise_module_1/classification_function.py
+++ week1_exercise_module_1/classification_function.py
@@ -1,14 +1,5 @@
-def check_type(TP, FP, FN):
-    if type(TP) != int:
-        return 'TP must be int'
-    elif type(FP) != int:
-        return 'FP must be int'
-    elif type(FN) != int:
-        return 'FN must be int'
-    else:
-        return TP, FP, FN
 def classification_function(TP, FP, FN):
-    check = check_type(TP, FP, FN)
+
     if type(TP) != int:
         return 'TP must be int'
     elif type(FP) != int:
@@ -23,7 +14,10 @@ def classification_function(TP, FP, FN):
         f1 = 2 * (precision * recall) / (precision + recall)
         return precision, recall, f1
 if __name__ == '__main__':
-    result = classification_function(2, 3 , 4)
-    print(f"Precision: {result[0]}")
-    print(f"Recall: {result[1]}")
-    print(f"F1 Score: {result[2]}")
+    result = classification_function(5, 3 , 4)
+    if type(result) == str:
+        print(result)
+    else:
+        print(f"Precision: {result[0]}")
+        print(f"Recall: {result[1]}")
+        print(f"F1 Score: {result[2]}")
