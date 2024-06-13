1bba1a1197cab0b726860d9241e09f6384225e11
VU TUAN ANH <96753460+tadongguk@users.noreply.github.com>
2024-06-13 12:24:48 +0900

    update_sonar_format

--- week2_exercise_module1/question1.py
+++ week2_exercise_module1/question1.py
@@ -1,6 +1,4 @@
 import random
-num_list = [3, 4, 5, 1, -44, 5, 10, 12, 33, 1]
-k = 3
 
 
 def k_large(num_list, k):
@@ -13,4 +11,6 @@ def k_large(num_list, k):
     return result
 
 
+num_list = [3, 4, 5, 1, -44, 5, 10, 12, 33, 1]
+k = 3
 print(k_large(num_list, k))
